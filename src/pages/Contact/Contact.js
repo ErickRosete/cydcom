9b334a182b74e0d9cbe6f16366b40b8abf0d005b
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2019-05-27 11:26:08 -0700

    changed contact phone

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -20,7 +20,7 @@ export class ContactPage extends Component {
                         <div className="contact__location-address">
                             <p>Paseo del Otoño No. 114, Lote 21, manzana XXII, 114, La Florida, Naucalpan, Estado de México</p>
                             <p>
-                                <span>(55) 8526-2318 | </span>
+                                <span>(55) 9627-8073 | </span>
                                 <span>ventas@epsilon.com | </span>
                                 <span>Lunes a Viernes: 9.00 am. - 7.00 pm.</span>
                             </p>
